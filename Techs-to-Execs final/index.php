ffc5560cbeddd17eff3d0af9a1f147939184bc99
Matt Campbell <matthewricecampbell@gmail.com>
2012-08-24 10:30:51 -0500

    Matt's Update

--- Techs-to-Execs final/index.php
+++ Techs-to-Execs final/index.php
@@ -1,10 +1,11 @@
+
 <?php get_header();?>
   <div role="main">
  <div id="container">
   <ul id="carousel">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
 <li> 
-<?php the_content(); ?>            
+  <?php the_content(); ?>  
 </li> 
 <?php endwhile; endif; ?>
 </ul>
--- Techs-to-Execs final/video.php
+++ Techs-to-Execs final/video.php
@@ -5,20 +5,19 @@ Template Name: Video (All)
 <?php get_header();?>
   <div role="main">
  <div id="container">
-  <div id="mainvideo">
+  
     <h3><?php the_title();?></h3>
+    <div id="mainvideo">
         <?php query_posts('post_type=video');
         if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
-<div id="title_video_thumbnails">
-<p><?php the_title();?></p><a href="<?php the_permalink();?>"><img id="video_all_thumbnails" src="http://img.youtube.com/vi/<?php echo get_post_meta( get_the_ID(), 'YOUR_PREFIX_video_url', true );?>/0.jpg"></a><?php endwhile; else: ?><!--/video_title_thumbnails-->
-<div style="clear: both;"></div>
-</div><!--/title_video_thumbnails-->
+<p id="video_titles"><?php the_title();?></p><a href="<?php the_permalink();?>"><img id="video_all_thumbnails" src="http://img.youtube.com/vi/<?php echo get_post_meta( get_the_ID(), 'YOUR_PREFIX_video_url', true );?>/0.jpg"></a><?php endwhile; else: ?>
 <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif;
 wp_reset_query(); ?>
-    
 <div style="clear: both;"></div>
 </div><!--/mainvideo-->
+
+
   
   <footer>
   </footer>
